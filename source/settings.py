4943f0be713c6d4dec2648fa9e5d9e01ea62df11
Pavel Durmanov <31005942+P-Alban@users.noreply.github.com>
2018-02-14 18:07:53 +0300

    Add files via upload

---
+++ source/settings.py
@@ -0,0 +1,10 @@
+# deley time for each site
+delay_time = 5
+# work time for alpha application
+alpha_time = 10
+# path to alpha application
+alpha_path = r"C:\Program Files\UltraISO\UltraISO.exe"
+# target domain
+target = 'www.alibaba.com/'
+# path to chromium
+chromium_path = r"C:\Users\user\Desktop\chrome-win32\chrome.exe"
