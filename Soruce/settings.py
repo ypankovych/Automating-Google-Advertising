e75510ec076d179b566dd0cb11bf72f3d54983a7
Pavel Durmanov <31005942+P-Alban@users.noreply.github.com>
2018-02-14 16:58:27 +0200

    Update settings.py

--- Soruce/settings.py
+++ Soruce/settings.py
@@ -3,8 +3,8 @@ delay_time = 5
 # work time for alpha application
 alpha_time = 10
 # path to alpha application
-alpha_path = r"C:\Program Files\UltraISO\UltraISO.exe"
+alpha_path = r""
 # target domain
 target = 'www.alibaba.com/'
 # path to chromium
-chromium_path = r"C:\Users\user\Desktop\chrome-win32\chrome.exe"
+chromium_path = r""
